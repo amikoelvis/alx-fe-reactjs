c4fd3e3db1110db317a16d873cf40d45286a6dcf
Amiko Elvis <amikoelvis@gmail.com>
2025-02-12 23:42:15 +0300

    modified App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-    <WelcomeMessage />
-  )
-  
+    <div>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  ); 
 }
 
 export default App
