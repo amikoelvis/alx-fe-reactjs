baac1825420e7da42ce3a074400190603cc4860f
Amiko Elvis <amikoelvis@gmail.com>
2025-02-21 16:58:00 +0300

    Apply Inline CSS Styling to React Components & Create a Simple Counter Application Using State and Hooks

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{textAlign: 'center', padding: '10px', margin: '10px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
