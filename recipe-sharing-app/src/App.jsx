5d8b06fa7150b6cf78d1c978baf4471c8bc79562
Amiko Elvis <amikoelvis@gmail.com>
2025-02-25 16:47:54 +0300

    final push

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,30 +1,32 @@
 // src/App.jsx
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
-      <h1 style={{ textAlign: 'center' }}>
-        <Link to="/" style={{ textDecoration: 'none', color: '#000' }}>
-          Recipe Sharing App
-        </Link>
-      </h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
+        <h1 style={{ textAlign: 'center' }}>
+          <Link to="/" style={{ textDecoration: 'none', color: '#000' }}>
+            Recipe Sharing App
+          </Link>
+        </h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
