1573b944ee501f276dba95aca9491ae431e1f25f
Amiko Elvis <amikoelvis@gmail.com>
2025-02-26 14:01:05 +0300

    final

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import useRecipeStore from './recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const EditRecipeForm = ({ recipe }) => {
   const updateRecipe = useRecipeStore((state) => state.updateRecipe);
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (!title.trim() || !description.trim()) return;
     updateRecipe({ ...recipe, title, description });
+    navigate(`/`);
   };
 
   return (
