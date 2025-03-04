5cd06315aaf2320787c627cd5af34f0e7910bf92
Amiko Elvis <amikoelvis@gmail.com>
2025-03-04 22:39:30 +0300

    11

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
-            <Route path="/blog/:postId" element={<BlogPost />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </BrowserRouter>
